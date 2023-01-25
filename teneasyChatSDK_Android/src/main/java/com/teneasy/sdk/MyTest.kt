687eda547cdc075b5b1911066336c6d473a06c5e
tianxuefeng <tianxuefeng2010@gmail.com>
2023-01-25 23:59:05 +0800

    connected

--- teneasyChatSDK_Android/src/main/java/com/teneasy/sdk/MyTest.kt
+++ teneasyChatSDK_Android/src/main/java/com/teneasy/sdk/MyTest.kt
@@ -2,15 +2,20 @@ package com.teneasy.sdk
 
 import android.content.Context
 import android.widget.Toast
-import dev.icerock.moko.socket.Socket
-import dev.icerock.moko.socket.SocketEvent
-import dev.icerock.moko.socket.SocketOptions
 import io.crossbar.autobahn.websocket.WebSocketConnection
 import io.crossbar.autobahn.websocket.WebSocketConnectionHandler
 import io.crossbar.autobahn.websocket.types.ConnectionResponse
+import org.java_websocket.client.WebSocketClient
+import org.java_websocket.drafts.Draft
+import org.java_websocket.drafts.Draft_6455
+import org.java_websocket.handshake.ServerHandshake
+import org.json.JSONObject
+import java.net.URI
 
 
 class MyTest {
+    //val url = "wss://127.0.0.1:8013/v1/gateway/h5?token=CAsQAxgBIAwojeyt6tQw.NcnU2L85lR8ImA3rTbHl5f8UCTueNQ8oyj7Kb4w2EEazoywQAHDoh5sxTOflvfUkgvWBuE3llWNvH5rDSLHCAQ"
+   val url = "wss://csapi.xdev.stream/v1/gateway/h5?token="
     fun sayHello(context: Context){
         Toast.makeText(context, "Good sdk! Good!", Toast.LENGTH_LONG).show()
     }
@@ -20,7 +25,7 @@ class MyTest {
 //        ws.connect();
 
         val connection = WebSocketConnection()
-        connection.connect("wss://127.0.0.1:8013/v1/gateway/h5?token=CAsQAxgBIAwojeyt6tQw.NcnU2L85lR8ImA3rTbHl5f8UCTueNQ8oyj7Kb4w2EEazoywQAHDoh5sxTOflvfUkgvWBuE3llWNvH5rDSLHCAQ", object : WebSocketConnectionHandler() {
+        connection.connect(url, object : WebSocketConnectionHandler() {
             override fun onConnect(response: ConnectionResponse) {
                 println("Connected to server")
                 Toast.makeText(context, "Connected to server", Toast.LENGTH_LONG).show()
@@ -43,51 +48,35 @@ class MyTest {
     }
 
     fun makeConnect2(){
-        val socket = Socket(
-            endpoint = "http://csapi.xdev.stream/v1/gateway/h5?token=",
-            config = SocketOptions(
-                queryParams = mapOf("token" to "MySuperToken"),
-                transport = SocketOptions.Transport.WEBSOCKET
-            )
-        ) {
-            on(SocketEvent.Connect) {
-                println("connected...")
-            }
 
-            on(SocketEvent.Connecting) {
-                println("connecting")
-            }
+        val mWs: WebSocketClient =
+            object : WebSocketClient(URI(url), Draft_6455()) {
+                override fun onMessage(message: String) {
+                    val obj = JSONObject(message)
+                    val channel = obj.getString("channel")
+                }
 
-            on(SocketEvent.Disconnect) {
-                println("disconnect")
-            }
+                override fun onOpen(handshake: ServerHandshake?) {
+                    println("opened connection")
+                }
 
-            on(SocketEvent.Error) {
-                println("error $it")
-            }
+                override fun onClose(code: Int, reason: String, remote: Boolean) {
+                    println("closed connection")
+                }
 
-            on(SocketEvent.Reconnect) {
-                println("reconnect")
+                override fun onError(ex: Exception) {
+                    ex.printStackTrace()
+                }
             }
-
-            on(SocketEvent.ReconnectAttempt) {
-                println("reconnect attempt $it")
-            }
-
-            on(SocketEvent.Ping) {
-                println("ping")
-            }
-
-            on(SocketEvent.Pong) {
-                println("pong")
-            }
-
-            on("employee.connected") { data ->
-               /* val serializer = DeliveryCar.serializer()
-                val json = JSON.nonstrict
-                val deliveryCar: DeliveryCar = json.parse(serializer, data)*/
-                //...
-            }
-        }
+        //open websocket
+        //open websocket
+        mWs.connect()
+        val obj = JSONObject()
+        obj.put("event", "addChannel")
+        obj.put("channel", "ok_btccny_ticker")
+        val message = obj.toString()
+        //send message
+        //send message
+        //mWs.send(message)
     }
 }
