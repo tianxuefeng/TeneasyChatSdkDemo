7058b33a6648820871a2381abc941ed0a9b01eec
tianxuefeng <tianxuefeng2010@gmail.com>
2023-02-15 14:43:36 +0800

    teneasy-chatsdk v1.0.9

--- app/src/main/java/com/example/teneasychatsdkdemo/MainActivity.kt
+++ app/src/main/java/com/example/teneasychatsdkdemo/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.example.teneasychatsdkdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.utils.widget.MotionButton
 import com.example.teneasychatsdkdemo.ui.main.MainFragment
+import com.teneasy.chatuisdk.ui.main.KeFuActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, MainFragment.newInstance())
+//                .commitNow()
+//        }
+
+        val btnKeFu =  findViewById<MotionButton>(R.id.btn_send)
+        btnKeFu.setOnClickListener({
+            val keFuIntent = Intent(this, KeFuActivity :: class.java)
+            this.startActivity(keFuIntent)
+        })
     }
 
 }
